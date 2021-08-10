43700152139228ad19060d1de8f24fd72448136b
Robert Sehr <robert.sehr@intranda.com>
2021-08-10 10:19:32 +0200

    fixed memory problem in plugin

--- plugin/src/main/java/de/intranda/goobi/plugins/RulesetCompatibilityAdministrationPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/RulesetCompatibilityAdministrationPlugin.java
@@ -1,9 +1,7 @@
 package de.intranda.goobi.plugins;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Stack;
 
 import org.goobi.beans.Process;
 import org.goobi.managedbeans.ProcessBean;
@@ -29,104 +27,106 @@ public class RulesetCompatibilityAdministrationPlugin implements IAdministration
     @Getter
     private String title = "intranda_administration_ruleset_compatibility";
 
-	@Getter @Setter
-	private int limit = 10;
+    @Getter @Setter
+    private int limit = 10;
 
-	@Getter
-	private int resultTotal = 0;
+    @Getter
+    private int resultTotal = 0;
 
-	@Getter
-	private int resultProcessed = 0;
+    @Getter
+    private int resultProcessed = 0;
 
-	@Getter 
-	private boolean run = false;
+    @Getter
+    private boolean run = false;
 
-	@Getter
-	@Setter
-	private String filter;
+    @Getter
+    @Setter
+    private String filter;
 
-	@Getter
-	private List<RulesetCompatibilityResult> resultsLimited = new ArrayList<RulesetCompatibilityResult>();
-	private List<RulesetCompatibilityResult> results = new ArrayList<RulesetCompatibilityResult>();
-	private PushContext pusher;
-	
-	/**
+    @Getter
+    private List<RulesetCompatibilityResult> resultsLimited = new ArrayList<>();
+    private List<RulesetCompatibilityResult> results = new ArrayList<>();
+    private PushContext pusher;
+
+    /**
      * Constructor
      */
     public RulesetCompatibilityAdministrationPlugin() {
         log.info("Ruleset compatiblity administration plugin started");
-		filter = ConfigPlugins.getPluginConfig(title).getString("filter", "");	
-    }   
+        filter = ConfigPlugins.getPluginConfig(title).getString("filter", "");
+    }
 
     /**
-	 * action method to run through all processes matching the filter
-	 */
-	public void execute() {
-		run = true;
-				
-		// filter the list of all processes that should be affected
-		String query = FilterHelper.criteriaBuilder(filter, false, null, null, null, true, false);
-		List<Process> tempProcesses = ProcessManager.getProcesses("prozesse.titel", query);
+     * action method to run through all processes matching the filter
+     */
+    public void execute() {
+        run = true;
 
-		resultTotal = tempProcesses.size();
-		resultProcessed = 0;
-		results = new ArrayList<RulesetCompatibilityResult>();
-		resultsLimited = new ArrayList<RulesetCompatibilityResult>();
-		
-		Runnable runnable = () -> {
-			try {
-				long lastPush = System.currentTimeMillis();
-				for (Process process : tempProcesses) {
-//					Thread.sleep(1000);
-					if (!run) {
-						break;
-					}
-					RulesetCompatibilityResult r = new RulesetCompatibilityResult();
-					r.setTitle(process.getTitel());
-					r.setId(process.getId());
-					try {
-						// Prefs prefs = process.getRegelsatz().getPreferences();
-						Fileformat ff = process.readMetadataFile();
-					} catch (Exception e) {
-						r.setStatus("ERROR");
-						r.setMessage(e.getMessage());
-					}
-					results.add(0, r);
-					if (results.size()>limit) {
-						resultsLimited = new ArrayList(results.subList(0, limit));											
-					} else {
-						resultsLimited = new ArrayList(results);		
-					}
-					resultProcessed++;
-					if (pusher != null && System.currentTimeMillis() - lastPush > 1000) {
-						lastPush = System.currentTimeMillis();
-						pusher.send("update");
-					}
-				}
+        // filter the list of all processes that should be affected
+        String query = FilterHelper.criteriaBuilder(filter, false, null, null, null, true, false);
+        List<Integer> tempProcesses = ProcessManager.getIDList( query);
 
-				run = false;
-				Thread.sleep(200);
-				if (pusher != null) {
-					pusher.send("update");
-				}
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		};
-		new Thread(runnable).start();
-	}
-	
-	/**
-	 * show the result inside of the process list
-	 * 
-	 * @return
-	 */
-	public String showInProcessList(String limit) {
+        resultTotal = tempProcesses.size();
+        resultProcessed = 0;
+        results = new ArrayList<>();
+        resultsLimited = new ArrayList<>();
+
+        Runnable runnable = () -> {
+            try {
+                long lastPush = System.currentTimeMillis();
+                for (Integer processId : tempProcesses) {
+                    Process process = ProcessManager.getProcessById(processId);
+
+                    //					Thread.sleep(1000);
+                    if (!run) {
+                        break;
+                    }
+                    RulesetCompatibilityResult r = new RulesetCompatibilityResult();
+                    r.setTitle(process.getTitel());
+                    r.setId(process.getId());
+                    try {
+                        // Prefs prefs = process.getRegelsatz().getPreferences();
+                        Fileformat ff = process.readMetadataFile();
+                    } catch (Exception e) {
+                        r.setStatus("ERROR");
+                        r.setMessage(e.getMessage());
+                    }
+                    results.add(0, r);
+                    if (results.size()>limit) {
+                        resultsLimited = new ArrayList<>(results.subList(0, limit));
+                    } else {
+                        resultsLimited = new ArrayList<>(results);
+                    }
+                    resultProcessed++;
+                    if (pusher != null && System.currentTimeMillis() - lastPush > 1000) {
+                        lastPush = System.currentTimeMillis();
+                        pusher.send("update");
+                    }
+                }
+
+                run = false;
+                Thread.sleep(200);
+                if (pusher != null) {
+                    pusher.send("update");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        new Thread(runnable).start();
+    }
+
+    /**
+     * show the result inside of the process list
+     * 
+     * @return
+     */
+    public String showInProcessList(String limit) {
         String search = "\"id:";
         for (RulesetCompatibilityResult r : results) {
-        	if (limit.isEmpty() || limit.equals(r.getStatus())) {
-        		search += r.getId() + " ";
-        	}
+            if (limit.isEmpty() || limit.equals(r.getStatus())) {
+                search += r.getId() + " ";
+            }
         }
         search += "\"";
         ProcessBean processBean = Helper.getBeanByClass(ProcessBean.class);
@@ -134,34 +134,34 @@ public class RulesetCompatibilityAdministrationPlugin implements IAdministration
         processBean.setModusAnzeige("aktuell");
         return processBean.FilterAlleStart();
     }
-	
+
     @Override
-	public PluginType getType() {
-		return PluginType.Administration;
-	}
-	
-	@Override
-	public String getGui() {
-		return "/uii/plugin_administration_ruleset_compatibility.xhtml";
-	}
+    public PluginType getType() {
+        return PluginType.Administration;
+    }
 
-	@Override
-	public void setPushContext(PushContext pusher) {
-		this.pusher = pusher;
-	}
-	
-	/**
-	 * get the progress in percent to render a progress bar
-	 * @return progress as percentage
-	 */
-	public int getProgress() {
-		return 100 * resultProcessed / resultTotal;	
-	}
+    @Override
+    public String getGui() {
+        return "/uii/plugin_administration_ruleset_compatibility.xhtml";
+    }
 
-	/**
-	 * stop further processing 
-	 */
-	public void cancel() {
-		run = false;
-	}
+    @Override
+    public void setPushContext(PushContext pusher) {
+        this.pusher = pusher;
+    }
+
+    /**
+     * get the progress in percent to render a progress bar
+     * @return progress as percentage
+     */
+    public int getProgress() {
+        return 100 * resultProcessed / resultTotal;
+    }
+
+    /**
+     * stop further processing
+     */
+    public void cancel() {
+        run = false;
+    }
 }
